11836c999fca141509fe82635278cc4325d06b1d
BlueTeaWolf <ole.hoerbel@gmail.com>
2023-03-26 20:00:15 +0200

    added top bar to summonerInformation.html cleanup ApiKey.java

--- src/main/java/de/blueteawolf/riotstats/ApiKey.java
+++ src/main/java/de/blueteawolf/riotstats/ApiKey.java
@@ -7,12 +7,10 @@ public class ApiKey {
     private static final String P_API_KEY = "";
 
     public String getAPI_KEY() {
-        String API_KEY = "?api_key=" + P_API_KEY;
-        return API_KEY;
+        return "?api_key=" + P_API_KEY;
     }
 
     public String getMATCH_API_KEY() {
-        String MATCH_API_KEY = "&api_key=" + P_API_KEY;
-        return MATCH_API_KEY;
+        return "&api_key=" + P_API_KEY;
     }
 }
